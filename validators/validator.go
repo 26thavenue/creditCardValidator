e60f9c733a14ff1a4f1749b224b86e551bbc8336
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2024-05-03 14:16:53 +0100

    feat:Added the validators

--- validators/validator.go
+++ validators/validator.go
@@ -1,1 +1,38 @@
 package validators
+
+import (
+	"strconv"
+)
+
+type CardNumber struct{ 
+	Number string
+
+}
+
+func ( c *CardNumber) validateCard() bool{
+
+	sum := 0
+	isSecond := false
+	length := len(c.Number)
+	
+
+	for i := length - 1 ; i>= 0; i--  {
+		digit, _ := strconv.Atoi(string(c.Number[i]))
+
+		if isSecond {
+            digit *= 2
+            if digit > 9 {
+                digit -= 9
+            }
+        }
+
+		sum += digit
+        isSecond = !isSecond
+
+	}
+
+	return sum % 10 == 0
+
+
+
+}
